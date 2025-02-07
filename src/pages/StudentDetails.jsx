76727ce5bb1b752745a6c8cad521a2f865e5c63b
Adabanya Christian <adabanyachristian21@gmail.com>
2025-02-07 10:51:54 +0100

    minor button tweaks

--- src/pages/StudentDetails.jsx
+++ src/pages/StudentDetails.jsx
@@ -15,26 +15,28 @@ const StudentDetail = () => {
         <div className="w-full max-w-lg bg-white shadow-lg rounded-lg p-8 relative">
           <div className="flex items-start border-b border-gray-300 pb-4">
             <div className="flex-1">
-              <h3 className="text-gray-800 text-xl font-bold">Student Details</h3>
+              <h3 className="text-gray-800 text-xl font-bold">
+                Student Details
+              </h3>
               <p className="text-gray-600 text-sm mt-1">
                 Provides a detailed summary of the student.
               </p>
             </div>
             <Link to="/students">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-3 ml-2 cursor-pointer shrink-0 fill-gray-400 hover:fill-red-500"
-              viewBox="0 0 320.591 320.591"
-            >
-              <path
-                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                data-original="#000000"
-              ></path>
-              <path
-                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                data-original="#000000"
-              ></path>
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-3 ml-2 cursor-pointer shrink-0 fill-gray-400 hover:fill-red-500"
+                viewBox="0 0 320.591 320.591"
+              >
+                <path
+                  d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+                  data-original="#000000"
+                ></path>
+                <path
+                  d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+                  data-original="#000000"
+                ></path>
+              </svg>
             </Link>
           </div>
 
@@ -56,25 +58,29 @@ const StudentDetail = () => {
                 Course <span className="ml-auto">{student.course}</span>
               </li>
               <li className="flex flex-wrap gap-4 text-sm ">
-                Enrollment Date <span className="ml-auto">{student.enrollmentDate}</span>
+                Enrollment Date{" "}
+                <span className="ml-auto">{student.enrollmentDate}</span>
               </li>
             </ul>
           </div>
 
           <div className="flex max-sm:flex-col items-center gap-4 mt-8">
-            <button
-              type="button"
-              className="text-sm px-4 py-2.5 w-full tracking-wide bg-transparent hover:bg-gray-50 text-gray-800 border border-gray-300 rounded-lg max-sm:order-1"
-            >
-              <Link to="/students">Cancel</Link>
-              
-            </button>
-            <button
-              type="button"
-              className="text-sm px-4 py-2.5 w-full tracking-wide bg-blue-600 hover:bg-blue-700 text-white rounded-lg"
-            >
-              <Link to={`/students/${student.id}/edit`}>Edit</Link>
-            </button>
+            <Link to="/students" className="w-full">
+              <button
+                type="button"
+                className="text-sm px-4 py-2.5 w-full tracking-wide bg-transparent hover:bg-gray-50 text-gray-800 border border-gray-300 rounded-lg max-sm:order-1"
+              >
+                Cancel
+              </button>
+            </Link>
+            <Link to={`/students/${student.id}/edit`} className="w-full">
+              <button
+                type="button"
+                className="text-sm px-4 py-2.5 w-full tracking-wide bg-blue-600 hover:bg-blue-700 text-white rounded-lg"
+              >
+                Edit
+              </button>
+            </Link>
           </div>
         </div>
       </div>
